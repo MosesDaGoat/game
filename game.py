708ceda1a32cc71de1f265bfd0a469928ace1fab
MosesDaGoat <Jeffersonfontil@gmail.com>
2024-09-12 14:26:37 -0400

    update background f

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame
-from sys import exit 
-
+import sys
+from scripts.util import load_image, load_images
 
 
 class Game:
@@ -9,24 +9,35 @@ class Game:
         pygame.init()
         
         pygame.display.set_caption("flappy bird")
-       
-        self.screen = pygame.display.set_mode((1200, 800))
-        self.display = pygame.Surface((600, 400))
         
-        
-        
+        self.clock = pygame.time.Clock()
+
+        self.screen = pygame.display.set_mode((500, 800))
+        self.display = pygame.Surface((643, 900))
+                
         clock = pygame.time.Clock()
         
         self.movement = [False, False]
 
-def run_game(self):
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-         
+        self.assets = {
+            'background': load_image('background/background.png'),
+        }
 
-        self.screen.blit(pygame.transform.scale(self.display,self.screen.get_size()),(0,0)),
-        pygame.display.update()
-        self.clock.tick(60)
+
+    def run(self):
+        
+        while True:
+            self.display.blit(self.assets['background'], (0, 0))
+
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+
+            self.screen.blit(pygame.transform.scale(self.display,self.screen.get_size()),(0,0)),
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
---
+++ scripts/util.py
@@ -0,0 +1,19 @@
+import os
+import pygame
+
+BASE_IMG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../game/images/')
+
+def load_image(path):
+    img = pygame.image.load(os.path.join(BASE_IMG_PATH, path)).convert()
+    img.set_colorkey((0, 0, 0))
+    return img
+
+def load_images(path):
+    images = []
+    full_path = os.path.join(BASE_IMG_PATH, path)
+    try:
+        for img_name in sorted(os.listdir(full_path)):
+            images.append(load_image(os.path.join(path, img_name)))
+    except FileNotFoundError:
+        print(f"Error: The path '{full_path}' does not exist.")
+    return images

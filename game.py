17692cb5ead5eae027fa1e5d3110d205a59ce0c0
MosesDaGoat <Jeffersonfontil@gmail.com>
2024-09-13 09:31:48 -0400

    refracter

--- game.py
+++ game.py
@@ -4,21 +4,23 @@ from scripts.util import load_image, load_images
 from scripts.gameconfig import GameConfig
 
 class Game:
+    SCREEN_SIZE = (700, 900)
+    DISPLAY_SIZE = (551, 720)
+    FPS = 60
+
     def __init__(self):
-        
         pygame.init()
-        
-        pygame.display.set_caption("flappy bird")
-        
+        self._setup_display()
+        self._load_assets()
+        self._setup_game_objects()
+
+    def _setup_display(self):
+        pygame.display.set_caption("Flappy Bird")
         self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode(self.SCREEN_SIZE)
+        self.display = pygame.Surface(self.DISPLAY_SIZE)
 
-        self.screen = pygame.display.set_mode((700, 900))
-        self.display = pygame.Surface((551, 720))
-                
-        clock = pygame.time.Clock()
-        
-        self.movement = [False, False]
-
+    def _load_assets(self):
         self.assets = {
             'background': load_image('background/background.png'),
             "bird": load_images('birdsprites'),
@@ -27,23 +29,37 @@ class Game:
             "floor": load_image('floor/ground.png')
         }
 
-        self.config = GameConfig(self.display.get_width(), self.display.get_height(), self.assets['floor'])
+    def _setup_game_objects(self):
+        self.config = GameConfig(self.DISPLAY_SIZE[0], self.DISPLAY_SIZE[1], self.assets['floor'])
 
     def run(self):
         while True:
-            self.display.blit(self.assets['background'], (0, 0))
+            self._handle_events()
+            self._update()
+            self._draw()
+            self._update_display()
 
-            self.config.update_ground()
-            self.config.draw_ground(self.display)
+    def _handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self._quit()
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-            
+    def _update(self):
+        self.config.update_ground()
 
-            self.screen.blit(pygame.transform.scale(self.display,self.screen.get_size()),(0,0)),
-            pygame.display.update()
-            self.clock.tick(60)
+    def _draw(self):
+        self.display.blit(self.assets['background'], (0, 0))
+        self.config.draw_ground(self.display)
 
-Game().run()
+    def _update_display(self):
+        scaled_display = pygame.transform.scale(self.display, self.SCREEN_SIZE)
+        self.screen.blit(scaled_display, (0, 0))
+        pygame.display.update()
+        self.clock.tick(self.FPS)
+
+    def _quit(self):
+        pygame.quit()
+        sys.exit()
+
+if __name__ == "__main__":
+    Game().run()
--- scripts/gameconfig.py
+++ scripts/gameconfig.py
@@ -7,24 +7,23 @@ class GameConfig:
         self.floor_image = floor_image
         self.scroll_speed = 4
         self.floor_y = screen_height - floor_image.get_height()
-        self.ground_objects = [self.create_ground(0)]
+        self.ground_width = floor_image.get_width()
+        self.ground_objects = [self._create_ground(0)]
 
-    def create_ground(self, x):
-        return {
-            'x': x,
-            'y': self.floor_y,
-            'image': self.floor_image
-        }
+    def _create_ground(self, x):
+        return {'x': x, 'y': self.floor_y}
+
+    def _get_last_ground_x(self):
+        return self.ground_objects[-1]['x'] if self.ground_objects else 0
 
     def update_ground(self):
         # Remove off-screen ground objects
-        self.ground_objects = [ground for ground in self.ground_objects if ground['x'] > -self.floor_image.get_width()]
+        self.ground_objects = [ground for ground in self.ground_objects if ground['x'] > -self.ground_width]
 
         # Add new ground objects if needed
-        if len(self.ground_objects) <= 2:
-            last_x = self.ground_objects[-1]['x'] + self.floor_image.get_width() if self.ground_objects else 0
-            new_ground = self.create_ground(last_x)
-            self.ground_objects.append(new_ground)
+        while len(self.ground_objects) < 3:  # Ensure we always have at least 3 ground objects
+            new_x = self._get_last_ground_x() + self.ground_width
+            self.ground_objects.append(self._create_ground(new_x))
 
         # Update ground positions
         for ground in self.ground_objects:
@@ -32,4 +31,4 @@ class GameConfig:
 
     def draw_ground(self, surface):
         for ground in self.ground_objects:
-            surface.blit(ground['image'], (ground['x'], ground['y']))
+            surface.blit(self.floor_image, (ground['x'], ground['y']))
